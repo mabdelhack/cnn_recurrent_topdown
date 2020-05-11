900f1f6b1ed0b2a1ddb31848d0e252f75975714a
mabdelhack <m.abouelyazid@gmail.com>
2020-05-11 14:46:36 -0500

    add cuda

--- loss/crossentropy_plus.py
+++ loss/crossentropy_plus.py
@@ -1,6 +1,6 @@
 from torch.nn import Module
 from torch.nn import functional as F
-from torch import zeros, mean
+from torch import zeros, mean, tensor
 
 
 class CrossEntropyLossPlus(Module):
@@ -12,14 +12,17 @@ class CrossEntropyLossPlus(Module):
         self.reduction = reduction
         self.recurrent_flag = recurrent_flag
         self.topdown_flag = topdown_flag
-        self.lambda_recognition = lambda_recognition
-        self.lambda_recurrent = lambda_recurrent
-        self.lambda_topdown = lambda_topdown
+        self.lambda_recognition = tensor(lambda_recognition)
+        self.lambda_recurrent = tensor(lambda_recurrent)
+        self.lambda_topdown = tensor(lambda_topdown)
 
     def forward(self, recognition_input, recurrent_input, topdown_input, target):
         recognition_loss = F.cross_entropy(recognition_input, target)
         recurrent_loss = zeros(1)
         topdown_loss = zeros(1)
+        if self.lambda_recognition.is_cuda:
+            recurrent_loss = recurrent_loss.cuda()
+            topdown_loss = topdown_loss.cuda()
         if self.recurrent_flag:
             for rec_input in recurrent_input:
                 if rec_input is None:
@@ -34,3 +37,9 @@ class CrossEntropyLossPlus(Module):
                self.lambda_recurrent * recurrent_loss + \
                self.lambda_topdown * topdown_loss
         return loss
+
+    def cuda(self, device):
+        super(CrossEntropyLossPlus, self).cuda(device)
+        self.lambda_recognition = self.lambda_recognition.cuda(device)
+        self.lambda_recurrent = self.lambda_recurrent.cuda(device)
+        self.lambda_topdown = self.lambda_topdown.cuda(device)
